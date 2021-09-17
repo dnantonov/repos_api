3e736c228249a42edf8ff9b6efd6dceee8e7fe7b
dnantonov <dnantonov@stud.etu.ru>
2021-09-17 11:55:01 +0300

    Update dockerfile

--- repos/settings.py
+++ repos/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-8ne!scz^=p44e&lrrnuhdzsm4$j!a5j7u)hakt3g34_m7no@ax
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0']
 
 
 # Application definition
