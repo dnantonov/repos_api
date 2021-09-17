e2dfca590cb2cfdfeec5f475d3100f38690310ef
dnantonov <dnantonov@stud.etu.ru>
2021-09-17 13:00:59 +0300

    Upload heroku.yml

--- repos/settings.py
+++ repos/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,12 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-8ne!scz^=p44e&lrrnuhdzsm4$j!a5j7u)hakt3g34_m7no@ax'
+SECRET_KEY = os.environ.get('SECRET_KEY', default='foo')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['0.0.0.0']
+ALLOWED_HOSTS = ['127.0.0.1', '0.0.0.0', 'https://peaceful-woodland-37293.herokuapp.com/']
 
 
 # Application definition
